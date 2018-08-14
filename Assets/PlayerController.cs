1799a46f0ad49b82ba76072f0954572bd9b83ac7
Bartek Lis <lisembart97@gmail.com>
2018-08-14 17:58:05 +0200

    Created project files and first simple prototype

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public int playerNumber;
+	private Rigidbody2D playerRgbd;
+	private JointMotor2D motor2d = default(JointMotor2D);
+	public float jumpFactor;
+	public float tekmeFactor = 800f;
+	public GameObject tekmebacak;
+
+	void Start () 
+	{
+		playerRgbd = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Jump();
+		}
+	}
+
+	public void Jump()
+	{
+		playerRgbd.AddForce(new Vector2(base.transform.up.x, Mathf.Abs(base.transform.up.y)) * this.jumpFactor);
+		TekmeCek();
+	}
+
+	private void TekmeCek()
+	{
+		this.motor2d.motorSpeed = this.tekmeFactor;
+		this.tekmebacak.GetComponent<HingeJoint2D>().motor = this.motor2d;
+	}
+}
