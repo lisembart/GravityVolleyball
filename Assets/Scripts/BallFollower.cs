2bf0199ef9d371765f40c1bf60165008785350f6
Bartek Lis <lisembart97@gmail.com>
2018-08-18 17:20:33 +0200

    Created BallFollower script and object

---
+++ Assets/Scripts/BallFollower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFollower : MonoBehaviour 
+{
+	public GameObject ballObject;
+	
+	void Update () 
+	{
+		transform.position = new Vector3(ballObject.transform.position.x, transform.position.y, transform.position.z);
+	}
+}
